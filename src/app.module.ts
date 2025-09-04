1385fb5ce444e67ccd8f5c8a1ec3d5ee1b255194
LegitAhmad <a.s20061111@gmail.com>
2025-09-04 19:46:39 +0500

    import mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import config from './config/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import config from './config/config';
       load: [config],
       expandVariables: true,
     }),
+    MongooseModule.forRoot(config().mongodbUri),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/config/config.ts
+++ src/config/config.ts
@@ -3,11 +3,17 @@ import process from 'process';
 export default () => {
   const nodeEnv = process.env.NODE_ENV;
   const port = process.env.PORT || '3000';
+  const mongodbUri = process.env.MONGODB_URI;
 
-  if (nodeEnv === undefined) {
+  if (!nodeEnv || !mongodbUri) {
     throw new Error(
       'The environment variables are required, but not specified correctly.\n Place the .env file at the root of your project.',
     );
   }
-  return { isDev: nodeEnv === 'development', nodeEnv: nodeEnv, port: port };
+  return {
+    mongodbUri: mongodbUri,
+    isDev: nodeEnv === 'development',
+    nodeEnv: nodeEnv,
+    port: port,
+  };
 };
