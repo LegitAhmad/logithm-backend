618f7c9157d84bf824a8ec7bef8385754cb9f1a7
LegitAhmad <a.s20061111@gmail.com>
2025-09-21 00:54:23 +0500

    make course endpoints

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
-@Controller('course')
-export class CourseController {}
+@Controller('courses')
+export class CourseController {
+  @Post()
+  async create() {}
+
+  @Get()
+  async getAll() {}
+
+  @Get(':id')
+  async getOne(@Param(':id') id: string) {}
+
+  @Patch(':id')
+  async update(@Param(':id') id: string) {}
+
+  @Delete(':id')
+  async remove(@Param(':id') id: string) {}
+}
