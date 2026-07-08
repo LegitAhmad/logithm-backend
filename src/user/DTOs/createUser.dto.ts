c8a60dcfb33c49df2fcf368e36301e116b6e7215
LegitAhmad <a.s20061111@gmail.com>
2026-07-09 00:26:09 +0500

    remove nestjs

--- src/user/DTOs/createUser.dto.ts
+++
@@ -1,9 +0,0 @@
-import z from 'zod';
-import { email, password } from './user.fields';
-
-export const CreateUserValidator = z.object({
-  email: email,
-  password: password,
-});
-
-export type CreateUserDto = z.infer<typeof CreateUserValidator>;
--- src/user/DTOs/user.fields.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const username = z.string().min(3).max(20);
-export const email = z.email();
-export const passwordHash = z.string();
-export const password = z.string().min(8);
-export const isVerified = z.boolean().default(false);
-export const firstName = z.string().min(3).max(20);
-export const lastName = z.string().min(3).max(20);
